8d685ef49e6694eeff2167472fa4735987429e39
will <denis.barbosa@fatec.sp.gov.br>
2022-02-24 12:24:09 -0300

    third task

--- src/App.js
+++ src/App.js
@@ -1,9 +1,39 @@
-import Estilos from "./Components/Estilos/Estilos";
-import MinMax from "./Components/MinMax/MinMax";
-import Titulo from "./Components/Titulo/Titulo";
-import Botao from "./Components/Botao/Botao";
+import Estilos from "./components/Estilos/Estilos";
+import MinMax from "./components/MinMax/MinMax";
+import Titulo from "./components/Titulo/Titulo";
+import Botao from "./components/Botao/Botao";
 import Contador from "./components/contador/Contador";
 import React from "react";
+
+
+
+function App() {
+  return (
+    <div className="App">
+
+      <Contador inicial={100} passo={10} limiteMax={150} limiteMin={50}/>
+      
+    </div>
+  );
+}
+export default App;
+
+
+
+
+
+
+
+
+
+
+/* <Estilos />
+      <MinMax min="2" max="10" />
+      <MinMax min={4} max={15} />
+      <Titulo principal="Olá" secundario="tudo bem" />
+      <Botao/> */
+
+
 
 // function App() {
 //   return (
@@ -14,22 +44,4 @@ import React from "react";
 
 //     </div>
 //   );
-// }
-
-function App() {
-  return (
-    <div className="App">
-
-      <Contador inicial={100} passo={10} />
-      
-    </div>
-  );
-}
-export default App;
-
-
-/* <Estilos />
-      <MinMax min="2" max="10" />
-      <MinMax min={4} max={15} />
-      <Titulo principal="Olá" secundario="tudo bem" />
-      <Botao/> */
+// }   
--- src/components/contador/Contador.js
+++ src/components/contador/Contador.js
@@ -2,15 +2,34 @@ import React, { useState } from 'react'
 
 
 export default props => {
+
     const [numero, setNumero] = useState(props.inicial)
 
-    const inc = () => setNumero(numero + props.passo)
-    const dec = () => setNumero(numero - props.passo)
+    const inc = () => {
+        if (numero >= props.limiteMax) {
+            console.log("Limite maximo ultrapassado");
+            return
+        }
+        setNumero(numero + props.passo)
+    }
+
+
+
+    const dec = () => {
+        if (numero <= props.limiteMin) {
+            console.log("Limite minimo ultrapassado")
+            return
+        }
+        setNumero(numero - props.passo)
+    }
+
+
+
     return (
         <div>
             <h2>{numero}</h2>
-            <button onclick={inc}>+</button>
-            <button onclick={dec}>-</button>
+            <button onClick={inc}>+</button>
+            <button onClick={dec}>-</button>
         </div>
 
     )//end return
