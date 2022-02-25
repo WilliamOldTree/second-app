2738c5fee89883de7f62865ec2906dc69c604ce6
will <denis.barbosa@fatec.sp.gov.br>
2022-02-25 20:28:04 -0300

    Atividade feita ate condicional 1

--- src/App.js
+++ src/App.js
@@ -1,22 +1,13 @@
-import Estilos from "./components/Estilos/Estilos";
-import MinMax from "./components/MinMax/MinMax";
-import Titulo from "./components/Titulo/Titulo";
-import Botao from "./components/Botao/Botao";
-import Contador from "./components/contador/Contador";
-import React from "react";
-
-
-
-function App() {
-  return (
-    <div className="App">
-
-      <Contador inicial={100} passo={10} limiteMax={150} limiteMin={50}/>
-      
-    </div>
-  );
-}
-export default App;
+// import Estilos from "./components/Estilos/Estilos";
+// import MinMax from "./components/MinMax/MinMax";
+// import Titulo from "./components/Titulo/Titulo";
+// import Botao from "./components/Botao/Botao";
+// import Contador from "./components/contador/Contador";
+// import Carro from "./components/carro/Carro";
+//import Pai from "./components/direta/Pai";
+// import Familia from "./components/Familia/Familia";
+// import Membros from "./components/Familia/Membros";
+// import ParImpar from "./components/parImpar/ParImpar";
 
 
 
@@ -26,6 +17,65 @@ export default App;
 
 
 
+
+
+
+
+// function App() {
+//   return (
+//     <div>
+//       <ParImpar num={3} />
+//       <ParImpar num={22} />
+//     </div>
+
+
+//   )
+// }
+// export default App;
+
+
+//++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+// function App() {
+
+//   return (
+//     <div>
+//     <Familia>
+//       <Membros nome="Will" sobrenome="Barbosa"/>
+//       <Membros nome="Elaine" sobrenome="Barbosa"/>
+//     </Familia>
+//     <Familia>
+//       <Membros nome="Raiza" sobrenome="Silva"/>
+//       <Membros nome="Miguel" sobrenome="Silva"/> 
+//     </Familia>
+//     </div> 
+//   );
+
+// }
+// export default App;
+
+//++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <Pai/>  
+//     </div>
+//   );
+// }
+// export default App;
+
+//++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+{/* <Contador inicial={100} passo={10} limiteMax={150} limiteMin={50}/>
+      <Carro modelo="Corolla" marca="Toyota" cor="branco" ano="2014" portas="4" combustivel="flex" cambio="automatico" aro='16 "' potencia="218 cv" preco="R$ 72000"/><br/>
+      <Carro modelo="Corolla" marca="Toyota" cor="branco" ano="2014" portas="4" combustivel="flex" cambio="automatico" aro='16 "' potencia="218 cv" preco="R$ 72000"/><br/>
+      <Carro modelo="Corolla" marca="Toyota" cor="branco" ano="2014" portas="4" combustivel="flex" cambio="automatico" aro='16 "' potencia="218 cv" preco="R$ 72000"/><br/>
+      <Carro modelo="Corolla" marca="Toyota" cor="branco" ano="2014" portas="4" combustivel="flex" cambio="automatico" aro='16 "' potencia="218 cv" preco="R$ 72000"/><br/> */}
+
+
+//++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 
 /* <Estilos />
       <MinMax min="2" max="10" />
@@ -33,7 +83,7 @@ export default App;
       <Titulo principal="Olá" secundario="tudo bem" />
       <Botao/> */
 
-
+//++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 
 // function App() {
 //   return (
@@ -45,3 +95,5 @@ export default App;
 //     </div>
 //   );
 // }   
+
+//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
